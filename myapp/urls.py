6743df5a081536de1b550e8d587fb94d173f5549
Your Name <you@example.com>
2020-05-09 00:49:14 +0900

    draft add

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('program/<int:pk>/', views.post_detail, name='post_detail'),
     path('program/new/', views.post_new, name='post_new'),
     path('program/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('program/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('program/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
