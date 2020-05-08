c1cf30be3883cbc90259f8d2d0253e34d7dc7c2c
Your Name <you@example.com>
2020-05-08 16:23:02 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Program
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     programs = Program.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'myapp/post_list.html', {'programs': programs})
+
+
+def post_detail(request, pk):
+    program = get_object_or_404(Program, pk=pk)
+    return render(request, 'myapp/post_detail.html', {'program': program})
